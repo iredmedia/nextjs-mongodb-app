35bd5b16f71660f56c78a47d1255c7d46a398281
Kevin Redman <iredmedia@gmail.com>
2019-11-24 16:42:08 -0800

    Fix pubsub issue

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS({
   env: {
+    PUBNUB_SUB_KEY: process.env.PUBNUB_SUB_KEY,
+    PUBNUB_PUB_KEY: process.env.PUBNUB_PUB_KEY,
     MONGODB_URI: process.env.MONGODB_URI,
     // CLOUDINARY_URL: process.env.CLOUDINARY_URL,
     DB_NAME: process.env.DB_NAME,
